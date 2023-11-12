9889269f6348bfdcbf6fc43cf695f20f1c8ebb5a
Junseo Oh <40460655+outsung@users.noreply.github.com>
2023-11-12 13:30:47 +0900

    Feat: use `notice` in `xam`

--- apps/xam/App.tsx
+++ apps/xam/App.tsx
@@ -1,10 +1,17 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { Federated } from "@callstack/repack/client";
+import { Suspense, lazy } from "react";
+
+const NoticeApp = lazy(() => Federated.importModule("notice", "./App"));
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Expo with Repack!</Text>
+      <Suspense fallback={<Text>Loading app1...</Text>}>
+        <NoticeApp />
+      </Suspense>
       <StatusBar style="auto" />
     </View>
   );
